f72ff0dd47c6c80adcc6788b297dd4e58d5a6167
GianptDev <gianptdev@gmail.com>
2022-09-12 20:21:24 +0200

    changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Between Api'
+copyright = '2022, _gianpy_'
+author = 'GianptDev (_gianpy_)'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.2.1'
+version = '1.2.1'
 
 # -- General configuration
 
